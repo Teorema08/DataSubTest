7aca9552653b6cf332fa49e641519d09aaf0b182
Teorema08 <64376961+Teorema08@users.noreply.github.com>
2021-12-10 16:36:28 +0300

    update b

--- task2/index.sql
+++ task2/index.sql
@@ -4,15 +4,19 @@
 -- a)
 SELECT Students.StudentId, FirstName, LastName
 FROM Students
-LEFT JOIN Exam ON Students.StudentId = Exam.StudentId
+INNER JOIN Exam ON Students.StudentId = Exam.StudentId
 WHERE CAST(Exam.Result AS int ) < 3
 GROUP BY Students.StudentId, FirstName, LastName
-HAVING count(Students.StudentId) > 2 
+HAVING count(*) >= 2 
 
 -- b)
-SELECT DISTINCT [Group]
-FROM Students
-LEFT JOIN Exam ON Students.StudentId = Exam.StudentId
-WHERE CAST(Exam.Result AS int ) < 3
-GROUP BY Students.StudentId, [Group]
-HAVING count(Students.StudentId) > 2 AND count([group]) > 10
+SELECT [Students.Group] 
+FROM Students 
+INNER JOIN 
+    ( SELECT Students.StudentId, COUNT(Exams.StudentId) AS NumberOfFailed
+       FROM Students INNER JOIN Exams ON Students.StudentId = Exams.StudentId
+       WHERE CAST(Exams.Result) < 3
+       GROUP BY Students.StudentId
+       ) AS FailedExams ON FailedExams.StudentId = Students.StudentId AND FailedExams.NumberOfFailed >= 2
+  GROUP BY [Students.Group]
+  HAVING COUNT(FailedExam.StudentId) >= 10
